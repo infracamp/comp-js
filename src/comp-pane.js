a6b47177281fa00eed94246cd87ab936cbd41d77
Matthias Leuffen <m@tth.es>
2019-02-21 01:10:02 +0100

    fix order of script loading

--- src/comp-pane.js
+++ src/comp-pane.js
@@ -5,18 +5,21 @@ class CompPane extends HTMLElement {
     constructor() {
         super();
         this.ajaxSrc = null;
+        this.templateNode = null;
+
     }
 
 
     reload() {
         var renderer = new Renderer();
-        renderer.renderInto(this, {}, $(this).find("template")[0]);
+        renderer.renderInto(this, {}, this.templateNode);
     }
 
 
     static get observedAttributes() { return ["ajax-src"]; }
 
     attributeChangedCallback(name, oldValue, newValue) {
+        console.log(this);
         switch (name) {
             case "ajax-src":
                 this.ajaxSrc = newValue;
@@ -24,9 +27,24 @@ class CompPane extends HTMLElement {
         }
     }
 
+
+
     connectedCallback() {
-        var renderer = new Renderer();
-        renderer.renderInto(this, {blah: "muh"}, $(this).find("template")[0]);
+        var self = this;
+        /* setTimeout(): make it work on chrome and opera to support dynamic instanciation - otherwise childElements will be empty*/
+        setTimeout( function () {
+            console.log("ready");
+            var renderer = new Renderer();
+            self.templateNode = self.firstElementChild;
+            console.log("connect", self.templateNode);
+            //this.templateNode = this.content.childNodes[0].cloneNode(true);
+
+            renderer.renderInto(self, {blah: "muh"}, self.templateNode);
+
+        }, 1);
+
+
+
     }
 
 
