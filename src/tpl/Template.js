7e301eaa38150aa974dc9da36ec058b5fdb23c34
Matthias <m@tth.es>
2019-04-05 14:14:53 +0200

    template test1 working

--- src/tpl/Template.js
+++ src/tpl/Template.js
@@ -8,49 +8,72 @@ function cj_render(source, target, scope) {
 
     var func = {
 
-        "for$": (source, target, expr) => {
-            console.log("expr", expr);
-            var curSource = source.cloneNode(false);
-            curSource.removeAttribute("for$");
-            var _eval = 'for (' + expr + ") { render(curSource, target); };";
+        "for$": (_r_source, _r_target, _r_expr) => {
+            console.log("expr", _r_expr);
+
+
+            var _eval = 'for (' + _r_expr + ") { render(_r_source, _r_target, true); };";
             eval(_eval);
             return false;
         },
-        "if$": (source, target, expr) => {
-            var curSource = source.cloneNode(false);
-            curSource.removeAttribute("for$");
-            var _eval = 'if (' + expr + ") { render(curSource, target) }";
+        "if$": (_r_source, _r_target, _r_expr) => {
+            var _eval = 'if (' + _r_expr + ") { render(_r_source, _r_target, true); };";
+            console.log (_eval);
             eval(_eval);
-            return true;
+            return false;
         },
-        "__eval__": (expr) => {
-
+        "__eval__": (_r_input) => {
+            _r_input = _r_input.replace(/\{\{(.*?)\}\}/g, (match, contents) => {
+                return eval(contents);
+            });
+            return _r_input;
         }
     };
 
-    var render = (source, target) => {
-        console.log (source);
-        if (source.nodeType !== 3)
-        for (curFunc in func) {
-            console.log(curFunc, source);
-            if (source.hasAttribute(curFunc)) {
+    var render = (source, target, noParseAttrs) => {
+        console.log ("node type", source.nodeType);
+        if (source.nodeType === 1) {
+            console.log("walk", source, target)
 
-                console.log("muh");
-                var ret = func[curFunc](source, target, source.getAttribute(curFunc));
-                if (ret === false)
-                    return;
+            if ( ! noParseAttrs) {
+                for (curFunc in func) {
+                    if (source.hasAttribute(curFunc)) {
+                        console.log(curFunc, source, target);
+                        var ret = func[curFunc](source, target, source.getAttribute(curFunc));
+                        if (ret === false)
+                            return;
+                    }
+                }
             }
-        }
-        console.log ("source", source);
-        for (var i = 0; i < source.childNodes.length; i++) {
-            var curSource = source.childNodes[i];
-            var newNode = curSource.cloneNode(false);
-            console.log("render", curSource);
-            target.appendChild(newNode);
-            render(newNode, curSource);
 
+
+            var newTarget = source.cloneNode(false);
+            target.appendChild(newTarget);
+
+            for (var i1 = 0; i1 < source.childNodes.length; i1++) {
+                var curSource1 = source.childNodes[i1];
+
+
+                // Render content nodes into previous target.
+                render(curSource1, newTarget);
+            }
+        } else if (source.nodeType === 3) {
+            var _new_elem = source.cloneNode(false);
+            _new_elem.textContent = func.__eval__(_new_elem.textContent);
+            target.appendChild(_new_elem);
+        } else {
+            //if ()
+            console.log ("normal node", source, target);
+            target.appendChild(source.cloneNode(false));
+            return;
         }
     };
     // @todo check if it is a template node and use contentElements
-    render(source, target);
+
+    // Walk all childs
+    for (var i = 0; i < source.childNodes.length; i++) {
+        var curSource = source.childNodes[i];
+        // Render content nodes into previous target.
+        render(curSource, target);
+    }
 }
--- www/inc/template.md
+++ www/inc/template.md
@@ -5,11 +5,26 @@
 ```html
 <div id="tpl">
     <p>I'm template</p>
+    <div id="l1">
+        <div id="l2">a</div>
+        <div id="l2b" if$="scope[0] == 1">neinb</div>
+        <div id="l2b" if$="scope[0] == 'a'">aja</div>
+    </div>
+    
     <div for$="data in scope">
         Hello {{ data }}
+        <div if$="data == 'opt'">
+            <div for$="cur in scope.opt">
+                Scope: {{ cur }}: {{ scope.opt[cur] }}
+            </div>
+        
+        </div>
     </div>
 </div>
+Target:
 <div id="target"></div>
+
+<pre id="debug"></pre>
 <script language="JavaScript">
 
 setTimeout(function() {
@@ -18,7 +33,12 @@ setTimeout(function() {
     
     console.log(tpl);
     
-    cj_render(tpl, target, ["a", "b", "c"]);
+    cj_render(tpl, target, {"arr": ["a", "b", "c"], "arr2": "b", "opt": {"a": "a1", "b": "b1"}});
+    setTimeout((e) => {
+        console.log($("#target").html());
+        $("#debug").text(target.innerHTML);
+    }, 100)
+    
 }, 100);
 
 
